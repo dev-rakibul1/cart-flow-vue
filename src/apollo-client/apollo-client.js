c4ffd1b83341792fb3b0fd2dacce73c63d96ac34
Rakibul Islam <fl.rakibul@gmail.com>
2024-12-27 19:43:36 +0600

    Update uri

--- src/apollo-client/apollo-client.js
+++ src/apollo-client/apollo-client.js
@@ -1,8 +1,8 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client/core";
 
 const httpLink = new HttpLink({
-  // uri: "https://cart-flow-graphql.onrender.com/", // Replace with your GraphQL endpoint
-  uri: "http://localhost:4000/", // Replace with your GraphQL endpoint
+  uri: "https://cart-flow-graphql.onrender.com/", // Replace with your GraphQL endpoint
+  // uri: "http://localhost:4000/", // Replace with your GraphQL endpoint
 });
 
 const cache = new InMemoryCache();
